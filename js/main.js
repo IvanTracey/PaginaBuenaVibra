6f2fb24ea5e0471009f0e714f6fd04ea4bc27012
IvoCode <ivtracey@gmail.com>
2026-05-10 15:56:28 -0300

    Carga datos de productos

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const productos=[
     img: "../img/aguas de florida.webp",
     descricion: "De 60ml, producto artesanal",
     precio: 5000,
-    categoria: "Agua de florida"
+    categoria: "Manifestacion",
     },
     {
     id:2,
@@ -15,56 +15,87 @@ const productos=[
     img: "../img/aguas de florida.webp",
     descricion: "De 100ml, producto artesanal",
     precio: 8000,
-    categoria: "Agua de florida"
+    categoria: "Manifestacion"
     },
     {
     id:3,
-    name:"Difusor de mirra",
+    name:"Difusor 100ml",
     img: "../img/difusores.webp",
-    descricion: "De 100ml, producto artesanal",
+    descricion: "Producto artesanal, 2 variedades",
     precio: 6000,
-    categoria: "Difusor"
-    },
-    {
-    id:4,
-    name:"Difusor de sandalo",
-    img: "../img/difusores.webp",
-    descricion: "De 100ml, producto artesanal",
-    precio: 6000,
-    categoria: "Difusor"
+    categoria: "Aromas",
+    variedades: ["Mirra", "Sandalo"]
     },
     {
     id:5,
     name:"Sahumerios artesanales",
     //relativa al HTML
-    img: "../img/sahumerios.webp",
+    img: "../img/sahumerios_artesanales.webp",
     descricion: "20cm de largo, 10 unidades",
     precio: 2000,
-    categoria: "Sahumerios"
+    categoria: "Defumacion",
+    variedad: ["Almizcle y benjui", "Citrus", "Lavanda", "Melisa", "Mirra", "Nag champa", "Romero", "Sandalo", "Vainilla y coco", "Verbena", "Verbena y melisa"]
     },
     {
     id:6,
     name:"Sahumerios importados",
-    img: "../img/sahumerios.webp",
-    descricion: "25cm de largo,, 15 unidades",
+    img: "../img/sahumerios_importados.webp",
+    descricion: "30cm de largo, 15 unidades",
     precio:3000,
-    categoria: "Sahumerios"
+    categoria: "Defumacion",
+    variedad: ["Atrae clientes", "Atrae dinero", "Black ice", "Citronella", "Cool water", "Fresa", "Lavanda", "Lick me all over", "Limpia hogar", "Manzana canela", "Meditacion", "Om", "Palo santo", "Rosa roja", "Rosa vainilla","Sandalo"]
     },
     {
     id:7,
     name:"Sahumerios importados",
-    img: "../img/sahumerios.webp",
+    img: "../img/sahumerios_importados.webp",
     descricion: "50cm de largo, 10 unidades",
     precio: 6500,
-    categoria: "Sahumerios"
+    categoria: "Defumacion",
+    variedad: ["Almizcle", "Atrae dinero", "Black ice", "Canela", "Citronella", "Coco", "Fresa", "Jazmin", "Lavanda", "Limpia hogar", "Meditacion", "Mirra", "Om", "Palo santo", "Sandalo"]
     },
     {
     id:8,
-    name:"Sahumos naturales",
+    name:"Sahumo mediano",
     img: "../img/sahumos.webp",
-    descricion: "aaaaaaa",
-    precio: 6500,
-    categoria: "Sahumos"
+    descricion: "Hecho con hierbas naturales, Xgr",
+    precio: 500,
+    categoria: "Defumacion",
+    variedades: ["Cedron", "Laurel", "Lavanda", "Pino alimonado y mandarina", "Pino alimonado y naranja", "Romero", "Salvia"]
+    },
+    {
+    id:9,
+    name:"Sahumo grande",
+    img: "../img/sahumos.webp",
+    descricion: "Hecho con Romero natural, Xgr",
+    precio: 1000,
+    categoria: "Defumacion",
+    },
+    {
+    id:10,
+    name:"Velas 7 chackras",
+    img: "../img/velas.webp",
+    descricion: "Velas de hornillo aromaticas ",
+    precio: 4500,
+    categoria: "Velas",
+    },
+    {
+    id:11,
+    name:"Velas aromaticas",
+    img: "../img/velas.webp",
+    descricion: "Lata de mandala 4.5cm",
+    precio: 6900,
+    categoria: "Velas",
+    variedades: ["Jazmin", "Lavanda", "Vainilla"]
+    },
+    {
+    id:12,
+    name:"Velas aromaticas",
+    img: "../img/velas.webp",
+    descricion: "Lata de mandala 6.5cm",
+    precio: 11200,
+    categoria: "Velas",
+    variedades: ["Jazmin", "Lavanda", "Vainilla"]
     },
     
 ];
